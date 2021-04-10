67a07c28f70b2dad37183a30b2aeb27085feae3a
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-04-10 12:57:48 +0200

    Update install-debug.sh

--- install-debug.sh
+++ install-debug.sh
@@ -372,7 +372,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/Sandokaaan/YIIMP
+    git clone https://github.com/Sandokaaan/YIIMP yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     sudo make
